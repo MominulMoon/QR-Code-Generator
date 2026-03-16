38f6dd30154214bbad6b956290b8c8b5f7f77aa6
MD Moon Babu <m.mmoon1527@gmail.com>
2026-03-17 00:44:21 +0600

    Added download option and some comments(used ai to add comments)

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ import QrCode from "qrcode";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// Small express app to handle QR code generation
 let app = express();
 const port = 2715;
 
+// These help us build file paths in an ES module
 let filename = fileURLToPath(import.meta.url);
 let dirname = path.dirname(filename);
 
+// Parse form data sent from the browser
 app.use(express.urlencoded({ extended: true }));
+
+// Serve the main HTML file from the public folder
 app.use(express.static(path.join(dirname, "public", "index.html")));
 
+// Show the form page
 app.get("/", (req, res) => {
   res.sendFile(path.join(dirname, "public", "index.html"));
 });
+
+// Create a QR code from the text the user sends
 app.post("/generate", (req, res) => {
   let { text } = req.body;
   QrCode.toDataURL(text).then((qrDataURL) => {
@@ -22,6 +30,9 @@ app.post("/generate", (req, res) => {
         <body style="text-align:center;font-family:sans-serif;">
           <h2>QR Code for: ${text}</h2>
           <img src="${qrDataURL}" style="animation: bounce 1s infinite;">
+          <br><br>
+          <!-- Simple button so the user can download the QR as an image -->
+          <a href="${qrDataURL}" download="qrcode.png">Download QR Code</a>
           <br><br>
           <a href="/">Generate Another</a>
           <style>
@@ -36,6 +47,7 @@ app.post("/generate", (req, res) => {
   });
 });
 
+// Start the server so we can visit it in the browser
 app.listen(port, () => {
   console.log(`Port is Runnig On http://localhost:${port}`);
 });
